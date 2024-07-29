a33b6aa36f464e3d9694bbdd6ac7a49564071474
Munkhgerelpurvee <164706050+Munkhgerelpurvee@users.noreply.github.com>
2024-07-29 20:36:38 +0800

    page blog hegiig username=bigsondev-iin medeelleer fetch hiiv.

--- blogapp/next.config.mjs
+++ blogapp/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'media.dev.to',
+          
+          },
+        ],
+      },
+
+
+};
 
 export default nextConfig;
--- blogapp/src/app/blogs/page.js
+++ blogapp/src/app/blogs/page.js
@@ -3,66 +3,113 @@ import Link from "next/link";
 import { useState, useEffect } from "react";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
-import { BlogCard } from "../../components/BlogCard";
+import Image from "next/image";
 
 
 
+const pageSize = 6;
 export default function BlogPage() {
 
     const [blogs, setBlogs] = useState([]);
+    const [page, setPage] = useState(1);
+    const [ended, setEnded] = useState(false);
 
 
     useEffect(() => {
-        const getData = async () => {
-            try{
-                const res = await fetch(`https://dev.to/api/articles`);
-                const data = await res.json();
-                setBlogs(data);
-
-            } catch(error){
-                console.log(error);
-            }
-        };
-
-        getData();
-
+       
+        LoadMore ()
     }, []);
 
 
 
+ function LoadMore () {
+
+
+         fetch(`https://dev.to/api/articles?username=bigsondev&page=${page}&per_page=${pageSize}`)
+             .then((res) => {
+               return res.json()
+            })
+             .then((data) => {
+                // Өмнөх дээрээ залгаж байна
+              const newBlogs = blogs.concat(data);
+              setBlogs(newBlogs);
+              setPage(page + 1);
+              if(data.length < pageSize) {
+                setEnded(true);
+              }
+            
+             });
+          
+    };
+        
+    
 
     return (<>
 
     <Navbar/>
+    <h1 className="mt-24 text-2xl font-bold p-4 mx-24">All blog post</h1>
+    <div className="container bg-yellow-500 mx-24">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 items-center">
+       
+        {/* {
+        art.map((item, index) => ( 
+          <div key={index}>{item.title}</div>
+          ))
+        } */}
+  
+        {
+  
+          blogs.map((el, id) => {
+            return(
 
-    <main className="flex flex-col mx-[100px] my-14 gap-8 md:max-w-[1200px] m-auto">
+                <Link key={el.id} href={`/blogs/${el.id}`}>
+              <div className="bg-slate-700 w-80 shadow-lg rounded-lg mb-2 p-4 ">
+            <Image src={el.social_image} width={500} height={500} />
+            <div className="flex">
+                <div className="bg-blue-400 rounded-xl px-2 my-2">{el.tag_list[0]}</div>
+
+            </div>
+              <Link href={el.url} target="_blank" key={el.id} className=" text-[#fff]">
+                {el.title}</Link>
+
+              <div className="flex gap-2 items-center mt-4 text-[#fff]">
+                <Image src={el.user.profile_image_90} width={30} height={50}/>
+                <div>{el.user.name}</div>
+                <div>{el.published_at}</div>
+
+              </div>
+              </div>
+              </Link>
+  
+            )
+          })
+        }
+  
+       
+      
+      </div>
+      {/* Хэрвээ төгссөн байх юм бол энэ товч харагдахаа болино */}
+      {
+      !ended && (
+      <div className="text-center mt-6"   onClick={LoadMore} >
+      <button
+        className="m-auto W-[200px] py-2 px-5 mb-[150px] bg-[#4B6BFB] text-slate-700 rounded-md"
+      >
+        Load More
+      </button>
+      </div>
+
+        )
+      }
+
+  
+
+    </div>
     
-    <h1 className="mt-10 text-2xl font-bold">All blog post</h1>
-        <div className="grid w-full grid-cols-1 gap-6 md:grid-cols-3">
-            {blogs.map((blog) => (
-                <Link key={blog.id} href={`/blogs/${blog.id}`}>
-                  <BlogCard
-                         key={blog.title}
-                         image={blog.social_image}
-                         title={blog.title}
-                         date={blog.published_at}
-                         tags={blog.tag_list}
-                   />
-                
-                </Link>
-
-
-            ))}
-
-
-        </div>
-
-
-
-    </main>
 
     <Footer />
     
-    
     </>)
 }
+
+    
