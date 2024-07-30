1d76fa4838b41aec46a67e04ccac1b5b0b7a0ffb
Munkhgerelpurvee <164706050+Munkhgerelpurvee@users.noreply.github.com>
2024-07-30 14:12:07 +0800

    blogs rout-iig container component-eer burev.

--- blogapp/src/app/blogs/page.js
+++ blogapp/src/app/blogs/page.js
@@ -4,112 +4,100 @@ import { useState, useEffect } from "react";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import Image from "next/image";
-
-
+import { Container } from "../../components/Container";
 
 const pageSize = 6;
 export default function BlogPage() {
+  const [blogs, setBlogs] = useState([]);
+  const [page, setPage] = useState(1);
+  const [ended, setEnded] = useState(false);
 
-    const [blogs, setBlogs] = useState([]);
-    const [page, setPage] = useState(1);
-    const [ended, setEnded] = useState(false);
+  useEffect(() => {
+    LoadMore();
+  }, []);
 
+  function LoadMore() {
+    fetch(
+      `https://dev.to/api/articles?username=bigsondev&page=${page}&per_page=${pageSize}`
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        // Өмнөх дээрээ залгаж байна
+        const newBlogs = blogs.concat(data);
+        setBlogs(newBlogs);
+        setPage(page + 1);
+        if (data.length < pageSize) {
+          setEnded(true);
+        }
+      });
+  }
 
-    useEffect(() => {
-       
-        LoadMore ()
-    }, []);
+  return (
+    <>
+      <Navbar />
 
+      <Container>
+        <div className="w-full px-4 h-fit">
+          <div className="flex flex-col items-center justify-center w-full gap-y-20 h-fit">
+            <h1 className="p-4 mx-24 mt-24 text-2xl font-bold">
+              All blog post
+            </h1>
 
-
- function LoadMore () {
-
-
-         fetch(`https://dev.to/api/articles?username=bigsondev&page=${page}&per_page=${pageSize}`)
-             .then((res) => {
-               return res.json()
-            })
-             .then((data) => {
-                // Өмнөх дээрээ залгаж байна
-              const newBlogs = blogs.concat(data);
-              setBlogs(newBlogs);
-              setPage(page + 1);
-              if(data.length < pageSize) {
-                setEnded(true);
-              }
-            
-             });
-          
-    };
-        
-    
-
-    return (<>
-
-    <Navbar/>
-    <h1 className="mt-24 text-2xl font-bold p-4 mx-24">All blog post</h1>
-    <div className="container bg-yellow-500 mx-24">
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 items-center">
-       
-        {/* {
+            <div className="grid items-center gap-4 md:grid-cols-2 lg:grid-cols-3">
+              {/* {
         art.map((item, index) => ( 
           <div key={index}>{item.title}</div>
           ))
         } */}
-  
-        {
-  
-          blogs.map((el, id) => {
-            return(
 
-                <Link key={el.id} href={`/blogs/${el.id}`}>
-              <div className="bg-slate-700 w-80 shadow-lg rounded-lg mb-2 p-4 ">
-            <Image src={el.social_image} width={500} height={500} />
-            <div className="flex">
-                <div className="bg-blue-400 rounded-xl px-2 my-2">{el.tag_list[0]}</div>
+              {blogs.map((el, id) => {
+                return (
+                  <Link key={el.id} href={`/blogs/${el.id}`}>
+                    <div className="p-4 mb-2 rounded-lg shadow-lg bg-slate-700 w-80 ">
+                      <Image src={el.social_image} width={500} height={500} />
+                      <div className="flex">
+                        <div className="px-2 my-2 bg-blue-400 rounded-xl">
+                          {el.tag_list[0]}
+                        </div>
+                      </div>
+                      <Link
+                        href={el.url}
+                        target="_blank"
+                        key={el.id}
+                        className=" text-[#fff]"
+                      >
+                        {el.title}
+                      </Link>
 
+                      <div className="flex gap-2 items-center mt-4 text-[#fff]">
+                        <Image
+                          src={el.user.profile_image_90}
+                          width={30}
+                          height={50}
+                        />
+                        <div>{el.user.name}</div>
+                        <div>{el.published_at}</div>
+                      </div>
+                    </div>
+                  </Link>
+                );
+              })}
             </div>
-              <Link href={el.url} target="_blank" key={el.id} className=" text-[#fff]">
-                {el.title}</Link>
-
-              <div className="flex gap-2 items-center mt-4 text-[#fff]">
-                <Image src={el.user.profile_image_90} width={30} height={50}/>
-                <div>{el.user.name}</div>
-                <div>{el.published_at}</div>
-
+            {/* Хэрвээ төгссөн байх юм бол энэ товч харагдахаа болино */}
+            {!ended && (
+              <div className="mt-6 text-center " onClick={LoadMore}>
+                <button className="m-auto W-[200px] py-2 px-5 mb-[150px] bg-[#4B6BFB] text-slate-700 rounded-md">
+                  Load More
+                </button>
               </div>
-              </div>
-              </Link>
-  
-            )
-          })
-        }
-  
-       
-      
-      </div>
-      {/* Хэрвээ төгссөн байх юм бол энэ товч харагдахаа болино */}
-      {
-      !ended && (
-      <div className="text-center mt-6"   onClick={LoadMore} >
-      <button
-        className="m-auto W-[200px] py-2 px-5 mb-[150px] bg-[#4B6BFB] text-slate-700 rounded-md"
-      >
-        Load More
-      </button>
-      </div>
+            )}
+          </div>
+        </div>
+      </Container>
 
-        )
-      }
-
-  
-
-    </div>
-    
-
-    <Footer />
-    
-    </>)
+      <Footer />
+    </>
+  );
 }
-
-    
--- blogapp/src/app/contactUs/page.js
+++ blogapp/src/app/contactUs/page.js
@@ -1,62 +1,79 @@
-
 "use client";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import Image from "next/image";
+import { Container } from "../../components/Container";
 
 export default function Page() {
-    return (
-       <>
-       
-       <Navbar />
+  return (
+    <>
+      <Navbar />
+
+      <Container>
         <main className="my-24 gap-8 md:min-w-[1200px] mx-24">
-
           <div>
-          <h1 className="my-8 mt-3 text-2xl font-bold mb-8 flex justify-center mx-24 md:max-w-[1200px]">Contact Us</h1>
-          <p className="mb-8 mx-24 md:max-w-[1200px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
-  
+            <h1 className="my-8 mt-3 text-2xl font-bold mb-8 flex justify-center mx-24 md:max-w-[1200px]">
+              Contact Us
+            </h1>
+            <p className="mb-8 mx-24  w-[600px] md:max-w-[1200px]">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam
+            </p>
           </div>
-  
+
           <div className="gap-[10px] mx-24 md:flex md:min-w-[1200px]">
-            <div  className="w-[345px] h-[133px]  p-8 border-2 border-slate-300   rounded-md mb-4  ">
-                  <h1 className="mb-3 font-bold">Address</h1>
-                   <p>1328 Oak Ridge Drive, Saint Louis, Missouri</p>
+            <div className="w-[345px] h-[133px]  p-8 border-2 border-slate-300   rounded-md mb-4  ">
+              <h1 className="mb-3 font-bold">Address</h1>
+              <p>1328 Oak Ridge Drive, Saint Louis, Missouri</p>
             </div>
-                      <div className="min-w-[345px] min-h-[133px] p-4  border-2 border-slate-300  rounded-md mb-8  ">
-                              <h1 className="mb-3 font-bold">Contact</h1>
-                                <p>313-332-8662 info@email.com</p>
-  
-                      </div>
-  
+            <div className="min-w-[345px] min-h-[133px] p-4  border-2 border-slate-300  rounded-md mb-8  ">
+              <h1 className="mb-3 font-bold">Contact</h1>
+              <p>313-332-8662 info@email.com</p>
+            </div>
           </div>
 
-          <div  className="min-w-[400px] p-4 mx-24 border-slate-500 bg-[#D1D5DB]  rounded-md md:max-w-[700px] mb-10 " >
-          <h1 className="my-8 mt-3 text-2xl font-bold">Leave a Message</h1>
-           <div className="gap-10 mb-4 md:flex">
-          <input class="placeholder:italic placeholder:text-slate-400 block bg-[#fff] w-full mb-4 md:min-w-20xl border border-slate-300 rounded-md py-1 pr-2 shadow-sm sm:text-sm" placeholder="Your Name" type="text" name="search"/> 
+          <div className="min-w-[400px] p-4 mx-24 border-slate-500 bg-[#D1D5DB]  rounded-md md:max-w-[700px] mb-10 ">
+            <h1 className="my-8 mt-3 text-2xl font-bold">Leave a Message</h1>
+            <div className="gap-10 mb-4 md:flex">
+              <input
+                class="placeholder:italic placeholder:text-slate-400 block bg-[#fff] w-full mb-4 md:min-w-20xl border border-slate-300 rounded-md py-1 pr-2 shadow-sm sm:text-sm"
+                placeholder="Your Name"
+                type="text"
+                name="search"
+              />
 
-          <input class="placeholder:italic placeholder:text-slate-400 block bg-[#fff] w-full mb-4 md:min-w-10xl border border-slate-300 rounded-md py-1 pr-2 shadow-sm sm:text-sm" placeholder="Your Email" type="email"name="search"/> 
+              <input
+                class="placeholder:italic placeholder:text-slate-400 block bg-[#fff] w-full mb-4 md:min-w-10xl border border-slate-300 rounded-md py-1 pr-2 shadow-sm sm:text-sm"
+                placeholder="Your Email"
+                type="email"
+                name="search"
+              />
+            </div>
 
-           </div>
-            
-            
-          <input class="placeholder:italic placeholder:text-slate-400 block bg-[#fff] w-full  border border-slate-300 rounded-md py-1 pr-2 shadow-sm sm:text-sm" placeholder="Subject" type="text" name="search"/> 
-          <br/>
+            <input
+              class="placeholder:italic placeholder:text-slate-400 block bg-[#fff] w-full  border border-slate-300 rounded-md py-1 pr-2 shadow-sm sm:text-sm"
+              placeholder="Subject"
+              type="text"
+              name="search"
+            />
+            <br />
 
-          <input class="placeholder:italic placeholder:text-slate-400 block bg-[#fff] w-full border border-slate-300 rounded-md p-16 shadow-sm sm:text-sm" placeholder="Write a massage" type="text"/> 
+            <input
+              class="placeholder:italic placeholder:text-slate-400 block bg-[#fff] w-full border border-slate-300 rounded-md p-16 shadow-sm sm:text-sm"
+              placeholder="Write a massage"
+              type="text"
+            />
 
-           <button
-            className="p-2 w-full md:flex md:w-[130px] md:justify-center bg-[#4B6BFB] mt-3 text-[#fff] text-sm rounded-md">
-             Send Message
-           </button>
-
+            <button className="p-2 w-full md:flex md:w-[130px] md:justify-center bg-[#4B6BFB] mt-3 text-[#fff] text-sm rounded-md">
+              Send Message
+            </button>
           </div>
-
-
-
         </main>
-        <Footer />
-       </>
-      
-);
-  }
+      </Container>
+      <Footer />
+    </>
+  );
+}
